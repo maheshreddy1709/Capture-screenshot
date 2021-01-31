6cc518748210fcc2394aeee2e45d1b4b707582c1
maheshreddy1709 <78289623+maheshreddy1709@users.noreply.github.com>
2021-01-31 10:08:35 +0530

    1. Capture screenshot laptop Primary display and save in perticular location with single click
    1. Capture screenshot laptop Primary display and save in perticular location with single click

---
+++ Captureimage/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Captureimage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFileBrowse = new System.Windows.Forms.TextBox();
+            this.btnFileBrowse = new System.Windows.Forms.Button();
+            this.opnfiledialog = new System.Windows.Forms.OpenFileDialog();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(253, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Capture";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(49, 9);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(198, 20);
+            this.txtName.TabIndex = 2;
+            this.txtName.Text = "sample";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "FileLoaciton:";
+            // 
+            // txtFileBrowse
+            // 
+            this.txtFileBrowse.Location = new System.Drawing.Point(73, 45);
+            this.txtFileBrowse.Name = "txtFileBrowse";
+            this.txtFileBrowse.Size = new System.Drawing.Size(171, 20);
+            this.txtFileBrowse.TabIndex = 4;
+            // 
+            // btnFileBrowse
+            // 
+            this.btnFileBrowse.Location = new System.Drawing.Point(253, 43);
+            this.btnFileBrowse.Name = "btnFileBrowse";
+            this.btnFileBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnFileBrowse.TabIndex = 5;
+            this.btnFileBrowse.Text = "Browse";
+            this.btnFileBrowse.UseVisualStyleBackColor = true;
+            this.btnFileBrowse.Click += new System.EventHandler(this.btnFileBrowse_Click);
+            // 
+            // opnfiledialog
+            // 
+            this.opnfiledialog.FileName = "openFileDialog1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(8, 73);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(10, 13);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 97);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnFileBrowse);
+            this.Controls.Add(this.txtFileBrowse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "GRL Screenshot Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFileBrowse;
+        private System.Windows.Forms.Button btnFileBrowse;
+        private System.Windows.Forms.OpenFileDialog opnfiledialog;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
---
+++ Captureimage/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
+
+namespace Captureimage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string filename = "sample";
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text != "")
+                filename = txtName.Text;
+            m_strFilename = txtFileBrowse.Text.Trim();
+            CaptureMyScreen();
+        }
+        private void CaptureMyScreen()
+
+        {
+
+            try
+
+            {
+
+                //Creating a new Bitmap object
+
+                Bitmap captureBitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+
+
+                //Bitmap captureBitmap = new Bitmap(int width, int height, PixelFormat);
+
+                //Creating a Rectangle object which will  
+
+                //capture our Current Screen
+
+                Rectangle captureRectangle = Screen.AllScreens[0].Bounds;
+
+
+
+                //Creating a New Graphics Object
+
+                Graphics captureGraphics = Graphics.FromImage(captureBitmap);
+
+
+
+                //Copying Image from The Screen
+
+                captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
+
+
+                lblStatus.Text = "-";
+                //Saving the Image File (I am here Saving it in My E drive).
+                if (m_strFilename != "")
+                {
+                    if (Directory.Exists(m_strFilename))
+                        Directory.CreateDirectory(m_strFilename);
+                    captureBitmap.Save(m_strFilename + "\\" + filename + ".jpg", ImageFormat.Jpeg);
+                    lblStatus.Text = "Captured";
+                }
+                else
+                {
+                    lblStatus.Text = "ERROR";
+                    MessageBox.Show("ERROR!");
+                }
+            }
+
+            catch (Exception ex)
+
+            {
+
+                MessageBox.Show(ex.Message);
+
+            }
+
+        }
+        string m_strFilename = "";
+        private void btnFileBrowse_Click(object sender, EventArgs e)
+        {
+            m_strFilename = "";
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtFileBrowse.Text = folderBrowserDialog1.SelectedPath;
+                m_strFilename = folderBrowserDialog1.SelectedPath;
+            }
+        }
+    }
+}
